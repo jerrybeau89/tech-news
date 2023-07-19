320c8c67c9d45835c75058d0c9fe363d9884d199
Jerry Beau Baggett <117130414+jerrybeau89@users.noreply.github.com>
2023-07-19 15:34:36 -0700

    set up login auth, API's for add, update, and delete, API's for upvotes and comments, and adding data to the dashboard templates

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from app.routes import home, dashboard
+from app.routes import home, dashboard, api
 from app.db import init_db
 from app.utils import filters
+
 
 
 def create_app(test_config=None):
@@ -19,6 +20,7 @@ def create_app(test_config=None):
     # register routes
     app.register_blueprint(home)
     app.register_blueprint(dashboard)
+    app.register_blueprint(api)
     app.jinja_env.filters['format_url'] = filters.format_url
     app.jinja_env.filters['format_date'] = filters.format_date
     app.jinja_env.filters['format_plural'] = filters.format_plural
